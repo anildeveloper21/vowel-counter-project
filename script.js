5fd169296ffef73c35c5f4ecff3f6b9bee026a42
anildeveloper21 <anildeveloper21@gmail.com>
2024-05-22 12:14:46 +0530

    vowel counter

---
+++ script.js
@@ -0,0 +1,19 @@
+const textarea = document.querySelector("#textarea");
+const button = document.querySelector("#count");
+const output = document.querySelector(".output");
+
+button.addEventListener("click", () => {
+    if(textarea.value === ''){
+        output.innerHTML = "please enter some text"
+    }else {
+        let val = textarea.value;
+        let count = 0;
+
+        for(let i=0; i<val.length; i++){
+            if(val[i] === "a" || val[i] === "e" || val[i] === "i" || val[i] === "o" || val[i] === "u"){
+                count++
+            }
+            output.innerHTML = `The Numbers of vowels are =  ${count}`;
+        }
+    }
+})
